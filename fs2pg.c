fa8f48c02ffb093ab4d8e6eadeadae8f73ae9f7d
yanagitani <takanori.yanagitani@gmail.com>
2020-03-15 06:59:15 +0000

    trying to read env. var...

--- fs2pg.c
+++ fs2pg.c
@@ -12,12 +12,16 @@ PG_FUNCTION_INFO_V1(id2path);
 Datum id2path(PG_FUNCTION_ARGS){
   text* id = PG_GETARG_TEXT_P(0);
 
-  text* path = (text*)palloc(VARSIZE(id));
-  SET_VARSIZE(path, VARSIZE(id));
+  const char* env_val = getenv("FS2PG_BASE_PATH");
+  const char* basedir = NULL == env_val ? "/pgdata/fs2pg" : env_val;
+  const uint16_t blen = strlen(basedir);
+  const uint16_t ilen = VARSIZE(id)-VARHDRSZ;
+  const uint16_t psz  = blen + ilen + 1 + VARHDRSZ;
+  text* idpath = (text*)palloc(psz);
+  SET_VARSIZE(idpath, psz);
+  snprintf(VARDATA(idpath), psz-VARHDRSZ, "%s/%.*s", basedir, ilen, VARDATA(id));
 
-  memcpy(VARDATA(path), VARDATA(id), VARSIZE(id)-VARHDRSZ);
-
-  PG_RETURN_TEXT_P(path);
+  PG_RETURN_TEXT_P(idpath);
 }
 
 PG_FUNCTION_INFO_V1(f2p_hsz); Datum f2p_hsz(PG_FUNCTION_ARGS){ PG_RETURN_INT32(VARHDRSZ); }
